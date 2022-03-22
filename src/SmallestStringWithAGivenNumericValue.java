29cfd8d52b1fd5ce64c25def7c629792628562d6
todd <jionglintao@gmail.com>
2022-03-22 11:52:25 +0000

    22-03

---
+++ src/SmallestStringWithAGivenNumericValue.java
@@ -0,0 +1,19 @@
+public class SmallestStringWithAGivenNumericValue {
+    /*
+    https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/
+     */
+    public String getSmallestString(int n, int k) {
+        char[] ch = new char[n];
+        for(int i=0;i<n;i++) {
+            ch[i]='a';
+            k--;
+        }
+        int currChar=0;
+        while(k>0) {
+            currChar=Math.min(25,k);
+            ch[--n]+=currChar;
+            k-=currChar;
+        }
+        return String.valueOf(ch);
+    }
+}
